41762e8f42e7aee59f1c91423c0151575340b959
Masayuki Morisaki <masayuki144@gmail.com>
2018-01-28 22:27:19 +0900

    update memo.

--- slide/jupyter-notebook/run.sh
+++ slide/jupyter-notebook/run.sh
@@ -1,2 +1,6 @@
 # jupyter notebook をdockerで立ち上げるコマンド
-docker run -it --rm -p 8888:8888 jupyter jupyter notebook --ip=0.0.0.0 --allow-root
+docker run -it --rm -v $(pwd)/notebook:/root/notebook -p 8888:8888 jupyter /bin/bash
+
+## docker container 内で実行するコマンド
+# cd
+# jupyter notebook --ip=0.0.0.0 --allow-root
