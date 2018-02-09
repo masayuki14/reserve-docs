6c2d04bdbf845dab737a77ee844c1ec0b35f110b
Masayuki Morisaki <masayuki144@gmail.com>
2018-02-09 22:06:37 +0900

    Add jupyter run script.

--- slide/jupyter-notebook/run.sh
+++ slide/jupyter-notebook/run.sh
@@ -1,6 +1,12 @@
+# Dockerfile をビルド
+docker build -t jupyter .
+
 # jupyter notebook をdockerで立ち上げるコマンド
-docker run -it --rm -v $(pwd)/notebook:/root/notebook -p 8888:8888 jupyter /bin/bash
+docker run -it --rm -v $(pwd)/notebook:/root/notebook -p 80:8888 jupyter
 
 ## docker container 内で実行するコマンド
-# cd
+# at /root/notebook
 # jupyter notebook --ip=0.0.0.0 --allow-root
+
+# 気象庁データダウンロード
+# http://www.data.jma.go.jp/gmd/risk/obsdl/index.php
